f723d88b8b14259b96e3daf662d24d378ae91819
Siriwipp <Siriwiphaauddee.mew@gmail.com>
2024-12-14 17:44:50 +0700

    Lab9

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int rows,int cols){
+	if(rows <= 0 || cols <= 0){
+	cout << "Invalid input"<< endl;
+}
+	for(int N = 0; N < rows; N++){
+		for(int M = 0; M < cols; M++){
+			cout << 'O';
+		}
+		cout <<'\n';
+	}
+	
+}
+
+
 
 int main(){
 	
